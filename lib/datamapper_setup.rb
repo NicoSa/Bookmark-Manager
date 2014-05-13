b66dc19af23f8363a880d85e4131d69fcdcb917b
Nico Saueressig <nico@nicosaueressig.de>
2014-05-13 15:45:02 +0100

    modified datamapper setup to allow database url env variable to specified

--- lib/datamapper_setup.rb
+++ lib/datamapper_setup.rb
@@ -1,12 +1,12 @@
 env = ENV["RACK_ENV"] || "development"
-database = ENV["DATABASE_URL"] || "postgres://localhost/bookmark_manager_#{env}"
+database_url = "postgres://localhost/bookmark_manager_#{env}"
 require 'data_mapper'
 require 'dm-timestamps'
 require_relative './link.rb'
 require_relative './tag.rb'
 require_relative './user.rb'
 
-DataMapper.setup(:default, database)
+DataMapper.setup(:default, ENV["DATABASE_URL"] || database_url )
 
 DataMapper.finalize
 
